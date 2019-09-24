c9d2197cb827151d3150cee9a57e52ba9b3da51d
OfficialKingJulian <julianorchard@hotmail.com>
2019-09-24 10:28:31 +0100

    Added rudimental PHP to begin with

--- index.php
+++ index.php
@@ -1,5 +1,44 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>WIP</title>
+</head>
+<body>
+  <h1>
+    List
+  </h1>
+    <?php
 
-  
+    $files = glob('/var/www/okju96.xyz/public_html/posts/*.html');
 
-?>
+    usort($files, function($a, $b) {return filemtime($a) < filemtime($b);});
+
+          foreach($files as $file) {
+            $file_name = substr($file, 41);
+            $file = substr($file, 35);
+    ?>
+
+        <div class="files">
+          <div>
+          <a href="<?php print $file; ?>">
+
+    <?php
+            print $file_name . "\n";
+    ?>
+
+          </a>
+          </div><!--
+      --><div>
+            <p>
+    <?php
+            print "last modified " . date ("F d Y". filemtime($file));
+    ?>
+            </p>
+          </div>
+      </div>
+    <?php
+      }
+    ?>
+
+</body>
+</html>
