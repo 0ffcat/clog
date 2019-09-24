6f9c00012a2ed2e52446d03564f7c2ba2233e805
OfficialKingJulian <julianorchard@hotmail.com>
2019-09-24 17:00:03 +0100

    last of today

--- index.php
+++ index.php
@@ -1,44 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>WIP</title>
+  <meta type="description" content="This blog is a template page. Please change literally everything." />
+  <title>Blog Testing</title>
+  <style>
+  <?php include_once("style.css"); ?>
+  </style>
 </head>
 <body>
+  <section>
   <h1>
-    List
+    Blog Post Testing
   </h1>
+    <div class="blogfeed">
     <?php
-
     $files = glob('/var/www/okju96.xyz/public_html/posts/*.html');
-
     usort($files, function($a, $b) {return filemtime($a) < filemtime($b);});
-
           foreach($files as $file) {
             $file_name = substr($file, 38);
             $file = substr($file, 31);
     ?>
 
-        <div class="files">
+        <div>
           <div>
-          <a href="<?php print "$file"; ?>">
-
-    <?php
-            print $file_name . "\n";
-    ?>
-
-          </a>
-          </div><!--
-      --><div>
             <p>
-    <?php
-            print "last modified " . date ("F d Y". filemtime($file));
-    ?>
+              <a href="<?php print "$file"; ?>">
+                <?php print $file_name . "\n"; ?>
+              </a>
             </p>
-          </div>
+        </div>
       </div>
     <?php
       }
     ?>
-
+    </div>
+  </section>
 </body>
 </html>
